f0a8cc050e9ad59315590e52b6d294472bcc6dc8
Daydream50 <471852478@qq.com>
2019-12-14 21:17:31 +0800

    test

---
+++ gmall-user/src/main/java/com/yuan/gmall/GmallUserApplication.java
@@ -0,0 +1,16 @@
+package com.yuan.gmall;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.yuan.gmall.user.mapper")
+public class GmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallUserApplication.class, args);
+    }
+
+}
